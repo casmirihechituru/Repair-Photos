275c70625823b75bdb1ef5a6fd6a557cb9ece4da
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-10-11 21:50:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def aboutus():
 def contactus():
     return render_template('contactus.html')
 
-@app.route('/subscribe')
+@app.route('/subscription')
 def privacypolicy():
     return render_template('payment.html')
     
