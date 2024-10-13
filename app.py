bc0ae898de4f16c81385f447b3382aab6a8b2999
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-10-13 17:19:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def payment():
     usr_uid = session['uid']
     email_for_paystack= db.child("users").child(usr_uid).child("email").get().val()
     return render_template('payment.html', email=email_for_paystack)
-'''
+
 def get_subscription_by_email(email):
     url = "https://api.paystack.co/subscription"
     headers = {
@@ -211,7 +211,7 @@ def check_subscription_status(subscription_code):
         else:
             return False
     return False
-
+'''
 #End of Added from My Chatbot GitHub code
 #*********************************************
 
